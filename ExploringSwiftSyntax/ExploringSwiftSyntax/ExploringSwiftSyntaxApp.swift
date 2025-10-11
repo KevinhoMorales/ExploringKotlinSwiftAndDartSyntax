85bf33e5993cacd13b054691af3de8f3ad9ca8d5
Kevinho Morales <kfmorales94@gmail.com>
2025-10-11 17:39:39 -0500

    Projects created

---
+++ ExploringSwiftSyntax/ExploringSwiftSyntax/ExploringSwiftSyntaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploringSwiftSyntaxApp.swift
+//  ExploringSwiftSyntax
+//
+//  Created by Kevinho Morales on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ExploringSwiftSyntaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
